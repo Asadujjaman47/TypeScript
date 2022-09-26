91307532dd4123e465d61694e91a3190fb43819c
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-26 20:48:18 +0600

    Classes

--- output/script.js
+++ output/script.js
@@ -1,4 +1,17 @@
 "use strict";
-let userDetails;
-userDetails = (id, userInfo) => {
-};
+class Player {
+    constructor(n, a, c) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
+    play() {
+        console.log(`${this.name} from ${this.country}  is playing!`);
+    }
+}
+;
+const mashrafi = new Player('Mashrafi', 40, 'Bangaldesh');
+const shakib = new Player('Shakib', 38, 'Bangaldesh');
+// Players array
+const Players = [];
+Players.push(mashrafi, shakib);
--- src/script.ts
+++ src/script.ts
@@ -1,12 +1,23 @@
-let userDetails: (id: number | string, userInfo: {
-    name: string,
-    age: number
-}) => void;
+class Player {
+    name: string;
+    age: number;
+    country: string;
 
-userDetails = (id: number | string, userInfo: {
-    name: string,
-    age: number,
-}) => {
+    constructor(n: string, a: number, c: string) {
+        this.name = n;
+        this.age = a;
+        this.country = c;
+    }
 
-}
+    play() {
+        console.log(`${this.name} from ${this.country}  is playing!`);
+    }
+};
 
+const mashrafi = new Player('Mashrafi', 40, 'Bangaldesh');
+const shakib = new Player('Shakib', 38, 'Bangaldesh');
+
+// Players array
+const Players:Player[] = [];
+
+Players.push(mashrafi, shakib);
