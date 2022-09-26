9990c6ebf87872886c7f7fcb444b7fdbdd7c0645
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-26 22:50:35 +0600

    interface

--- output/script.js
+++ output/script.js
@@ -3,3 +3,13 @@ const mashrafi = new Player('Mashrafi', 40, 'Bangaldesh');
 const sakib = new Player('Sakib', 38, 'Bangaldesh');
 console.log(sakib.age);
 console.log(sakib.country);
+function drawRectangle(options) {
+    let width = options.width;
+    let length = options.length;
+}
+let threeDoptions = {
+    width: 30,
+    length: 20,
+    height: 10,
+};
+drawRectangle(threeDoptions);
--- src/script.ts
+++ src/script.ts
@@ -5,3 +5,21 @@ const sakib = new Player('Sakib', 38, 'Bangaldesh');
 
 console.log(sakib.age);
 console.log(sakib.country);
+
+// define interface 
+interface RectangleOptions {
+    width: number;
+    length: number;
+}
+function drawRectangle(options: RectangleOptions) {
+    let width = options.width;
+    let length = options.length;
+}
+
+let threeDoptions = {
+    width: 30,
+    length: 20,
+    height: 10,
+};
+
+drawRectangle(threeDoptions);
