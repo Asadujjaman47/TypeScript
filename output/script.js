ab573087c0c9c59d386358585d603805ec5cfe1e
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-26 21:33:46 +0600

    Module System

--- output/script.js
+++ output/script.js
@@ -1,20 +1,5 @@
-"use strict";
-class Player {
-    constructor(name, age, country) {
-        this.name = name;
-        this.age = age;
-        this.country = country;
-    }
-    play() {
-        console.log(`${this.name} from ${this.country}  is playing!`);
-    }
-}
-;
+import { Player } from './classes/Player.js';
 const mashrafi = new Player('Mashrafi', 40, 'Bangaldesh');
 const sakib = new Player('Sakib', 38, 'Bangaldesh');
-sakib.name = "Mashrafi";
-sakib.age = 40;
-sakib.country = 'England';
-console.log(sakib.name);
 console.log(sakib.age);
 console.log(sakib.country);
--- src/script.ts
+++ src/script.ts
@@ -1,22 +1,7 @@
-class Player {
-
-    constructor(
-        private name: string,
-        public age: number,
-        readonly country: string,
-    ) {}
-
-    play() {
-        console.log(`${this.name} from ${this.country}  is playing!`);
-    }
-};
+import { Player } from './classes/Player.js';
 
 const mashrafi = new Player('Mashrafi', 40, 'Bangaldesh');
 const sakib = new Player('Sakib', 38, 'Bangaldesh');
 
-sakib.name = "Mashrafi";
-sakib.age = 40;
-sakib.country = 'England';
-console.log(sakib.name);
 console.log(sakib.age);
 console.log(sakib.country);
