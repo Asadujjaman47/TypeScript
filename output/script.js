7b30f8c8e2628bbbe3302cffde653af1f5f017a9
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-26 11:36:28 +0600

    object

--- output/script.js
+++ output/script.js
@@ -5,3 +5,12 @@ b = "Asad";
 // array
 let a = [];
 a.push("Asad", 4, false);
+// object
+let c;
+c = {
+    name: "Asad",
+    age: 24,
+    adult: true
+};
+let d;
+d = [1, 2, 3];
--- src/script.ts
+++ src/script.ts
@@ -9,3 +9,21 @@ let a: (string | number)[] = [];
 
 a.push("Asad", 4, false);
 
+// object
+let c: {
+    name: string,
+    age: number,
+    adult: boolean
+};
+
+c = {
+    name: "Asad",
+    age: 24,
+    adult: true
+}
+
+
+let d: object;
+
+d = [1, 2, 3];
+
