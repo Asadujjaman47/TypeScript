9913a5dc3828bd5b8eea7f1e7e550125793e714d
Asadujjaman <asadujjaman15-10212@diu.edu.bd>
2022-09-26 11:38:05 +0600

    Dynamic Type

--- output/script.js
+++ output/script.js
@@ -1,16 +1,1 @@
 "use strict";
-// normal variable
-let b;
-b = "Asad";
-// array
-let a = [];
-a.push("Asad", 4, false);
-// object
-let c;
-c = {
-    name: "Asad",
-    age: 24,
-    adult: true
-};
-let d;
-d = [1, 2, 3];
